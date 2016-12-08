cc62793b062471c1e3ac923ef3ef4d57d4180823
xiaotingamy <502150736@qq.com>
2016-12-08 08:19:16 +0800

    products

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //app.js
 App({
+  // onLaunch 小程序初始化 （全局只触发一次）
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
+  // 开发者可以添加任意的函数 或者数据，通过this可以访问
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
@@ -24,7 +26,19 @@ App({
       })
     }
   },
+  // 数据对象  通过this可以访问
   globalData:{
     userInfo:null
   }
 })
+
+// 注意之处：
+// App() 必须在 app.js 中注册，且不能注册多个。
+
+//不要在定义于 App() 内的函数中调用 getApp() ，
+//使用 this 就可以拿到 app 实例。
+
+//不要在 onLaunch 的时候调用 getCurrentPage()，
+//此时 page 还没有生成。
+
+//通过 getApp() 获取实例之后，不要私自调用生命周期函数。
--- pages/product/list/list.js
+++ pages/product/list/list.js
@@ -1,5 +1,60 @@
 Page({
     data: {
+        schedule: 50
         
+    },
+     onLoad: function(options) {
+        // Do some initialize when page load.
+        console.log('ss');
+    },
+    canvasIdErrorCallback: function (e) {
+        console.error(e.detail.errMsg)
+    },
+    onReady: function (e) {
+
+        // 使用 wx.createContext 获取绘图上下文 context
+        var context = wx.createContext()
+
+        // context.setStrokeStyle("#00ff00")
+        // context.setLineWidth(5)
+        // context.rect(0, 0, 200, 200)
+        // context.stroke()
+        // context.setStrokeStyle("#ff0000")
+        // context.setLineWidth(2)
+        // context.moveTo(160, 100)
+        // context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+        // context.moveTo(140, 100)
+        // context.arc(100, 100, 40, 0, Math.PI, false)
+        // context.moveTo(85, 80)
+        // context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+        // context.moveTo(125, 80)
+        // context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+        // context.stroke()
+        var canvas_w = 100;
+        var canvas_h = 100;
+        var line_w = 4;
+        context.beginPath();
+        context.arc(canvas_w / 2, canvas_h / 2, canvas_w / 2 - line_w, 0, Math.PI * 2,false);
+        context.lineWidth = line_w;
+        context.strokeStyle = '#d8d7d7';
+        context.stroke();
+        context.closePath();
+        context.beginPath();
+        context.arc(canvas_w / 2, canvas_h / 2, canvas_w / 2 - line_w, -Math.PI * 0.5, Math.PI * 2 * this.data.schedule / 100 - Math.PI * 0.5, false);
+        context.strokeStyle = '#ee5b4c';
+        context.stroke();
+        context.closePath();
+        context.font = 'bold 32px Arial';  
+        context.fillStyle = '#e74c3c';
+        context.textAlign = 'center';  
+        context.textBaseline = 'middle';  
+        context.moveTo(canvas_w / 2, canvas_h / 2);  
+        // context.fillText(text, canvas_w / 2, canvas_h / 2);
+
+        // 调用 wx.drawCanvas，通过 canvasId 指定在哪张画布上绘制，通过 actions 指定绘制行为
+        wx.drawCanvas({
+            canvasId: 'firstCanvas',
+            actions: context.getActions() // 获取绘图动作数组
+        })
     }
 })
